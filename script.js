77841a42876585ed704fe30e4d7bcd158f7658b5
GramBam <graham.moss@hotmail.com>
2017-12-19 17:00:12 -0500

    add Flex Grow button and functions

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ $(document).ready(function() {
   //wrap
 
   $('.wrap').click(function() {
+    $('.box').css("width", "33.333%");
     $('.container').css('flex-wrap', 'wrap');
   });
 
   $('.wrap-reverse').click(function() {
+    $('.box').css("width", "33.333%");
     $('.container').css('flex-wrap', 'wrap-reverse');
   });
 
   $('.nowrap').click(function() {
     $('.container').css('flex-wrap', 'nowrap');
+    $('.box').css("width", "auto");
   });
 
   //direction
@@ -27,9 +30,51 @@ $(document).ready(function() {
   //justify
 
   $('.space-between').click(function() {
-    $('.box').css("width", "100px");
+    $('.box5').css('flex-grow', '0');
+    $('.box10').css('flex-grow', '0');
+    $('.box').css("width", "auto");
     $('.container').css('justify-content', 'space-between');
   });
+
+  $('.space-around').click(function() {
+    $('.box5').css('flex-grow', '0');
+    $('.box10').css('flex-grow', '0');
+    $('.box').css("width", "auto");
+    $('.container').css('justify-content', 'space-around');
+  });
+
+  $('.flex-center').click(function() {
+    $('.box5').css('flex-grow', '0');
+    $('.box10').css('flex-grow', '0');
+    $('.box').css("width", "auto");
+    $('.container').css('justify-content', 'center');
+  });
+
+  $('.flex-start').click(function() {
+    $('.box5').css('flex-grow', '0');
+    $('.box10').css('flex-grow', '0');
+    $('.box').css("width", "auto");
+    $('.container').css('justify-content', 'flex-start');
+  });
+
+  $('.flex-end').click(function() {
+    $('.box5').css('flex-grow', '0');
+    $('.box10').css('flex-grow', '0');
+    $('.box').css("width", "auto");
+    $('.container').css('justify-content', 'flex-end');
+  });
+
+  //grow
+
+  $('.flex-grow').click(function() {
+    $('.box5').css('flex-grow', '1');
+  });
+
+  $('.flex-grow2').click(function() {
+    $('.box5').css('flex-grow', '1');
+    $('.box10').css('flex-grow', '2');
+  });
+
 
   //reset
 
